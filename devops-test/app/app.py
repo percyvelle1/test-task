33c9308dd76586ea75ab59b275be057f44ade16b
seifolla <percyvellew1@gmail.com>
2025-09-23 00:46:30 +0500

    Add files via upload

---
+++ devops-test/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+@app.route("/")
+def hello():
+    return "Hello, World! From container."
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "3000"))
+    app.run(host="0.0.0.0", port=port)
