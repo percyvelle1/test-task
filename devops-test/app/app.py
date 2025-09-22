c1924351906b5ad9331e8987d8c7f3cb2a7c2b49
seifolla <percyvellew1@gmail.com>
2025-09-23 02:32:37 +0500

    Update app.py

--- devops-test/app/app.py
+++ devops-test/app/app.py
@@ -9,7 +9,7 @@ def health():
 
 @app.route("/")
 def hello():
-    return "Hello World! from test task container."
+    return "Hello World! from the test task container."
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "3000"))
